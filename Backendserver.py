2fc5bb2b7c54bf818b975ed0380271a8a2aa9318
Reinhard Wabwoba <renny.joshw@gmail.com>
2026-04-24 14:50:09 -0700

    Refactor app.run and db creation context
    Comment out app.run and add context for db creation.

--- Backendserver.py
+++ Backendserver.py
@@ -96,7 +96,9 @@ if __name__ == "__main__":
         
 with app.app_context():
     db.create_all()
-    app.run(host="0.0.0.0", port=5000)
+    with app.app_context():
+    db.create_all()
+   # app.run(host="0.0.0.0", port=5000)
  #   socketio.run(
      #   app,
       #  host="0.0.0.0",   # 🔴 THIS IS THE KEY FIX
