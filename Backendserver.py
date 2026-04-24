4b526be5f702d12701a7080f61aa53bd15f5524d
Reinhard Wabwoba <renny.joshw@gmail.com>
2026-04-24 14:42:00 -0700

    Fix indentation for app context in Backendserver.py

--- Backendserver.py
+++ Backendserver.py
@@ -93,7 +93,9 @@ def get_vehicles():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-
+        
+with app.app_context():
+    db.create_all()
     app.run(host="0.0.0.0", port=5000)
  #   socketio.run(
      #   app,
